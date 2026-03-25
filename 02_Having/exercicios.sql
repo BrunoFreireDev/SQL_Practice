65be05c181da62d8c3d54697cdc4b16fb38065e7
Bruno Freire <brunofreiresilva2001@gmail.com>
2026-03-25 16:42:08 -0300

    Realizando exercicios de having

--- 02_Having/exercicios.sql
+++ 02_Having/exercicios.sql
@@ -1,4 +1,4 @@
-/*Dicas rápidas para esses exercícios (sem entregar o código):
+/*Dicas rápidas para esses exercícios:
 
 Sempre use GROUP BY na coluna(s) que você está agrupando (ex.: cidade, nome do cliente, forma, etc.).
 O HAVING vem depois do GROUP BY e filtra os grupos (não as linhas individuais).
@@ -48,16 +48,54 @@ HAVING    COUNT(pro.produto) > 5
 ORDER BY  cat.descricao;
 
 -- 144. Liste as formas de pagamento que foram usadas em mais de 4 pagamentos (forma e contagem).
+SELECT   forma        AS "Formas de pagamento"
+        ,COUNT(forma) AS "Vezes usada"
+FROM     pagamentos
+GROUP BY forma
+HAVING   COUNT(forma) > 4;
 
 -- 145. Mostre os pedidos que tiveram mais de 5 itens diferentes (número do pedido e contagem distinta de produtos).
+SELECT   ped.id_pedido AS "Pedido"
+        ,COUNT(DISTINCT ped.id_produto) AS "Produtos diferentes no pedido"
+FROM     ped_itens ped
+GROUP BY ped.id_pedido
+HAVING   COUNT(DISTINCT ped.id_produto) > 5; 
 
 -- 146. Liste os clientes cujo total gasto em pedidos é maior que R$ 2.000,00 (nome e soma dos totais).
+SELECT   cli.nome       AS "Cliente"
+        ,SUM(ped.total) AS "Total gasto em pedidos"
+FROM     clientes cli
+JOIN     pedidos ped
+  ON     cli.idcliente = ped.id_cliente
+GROUP BY cli.idcliente
+HAVING   SUM(ped.total) > 2000.00;
 
 -- 147. Mostre os produtos que foram vendidos mais de 10 vezes no total (nome do produto e contagem de linhas em ped_itens).
+SELECT   pro.produto
+        ,COUNT(ped.id_produto) AS "Quantidade vendida"
+FROM     produtos pro
+JOIN     ped_itens ped
+  ON     ped.id_produto = pro.idproduto
+GROUP BY pro.produto
+HAVING   COUNT(ped.id_produto) > 10;
 
 -- 148. Liste os meses (ano e mês) em que foram emitidos mais de 5 pedidos (ex.: '2024-05' e contagem).
+SELECT    EXTRACT(YEAR FROM emissao) || '/' || EXTRACT(MONTH FROM emissao) AS Período
+         ,COUNT(idpedido)
+FROM      pedidos
+GROUP BY  EXTRACT(YEAR FROM emissao) || '/' || EXTRACT(MONTH FROM emissao)
+HAVING    COUNT(idpedido) > 5;
+/*O '||' não concatena no MySQL, então lembre de usar CONCAT() e não errar que nem eu*/
+
+-- Correção:
+SELECT    CONCAT(EXTRACT(YEAR FROM emissao),'/', EXTRACT(MONTH FROM emissao)) AS Período
+         ,COUNT(idpedido) AS "Quantidade de pedidos no período"
+FROM      pedidos
+GROUP BY  CONCAT(EXTRACT(YEAR FROM emissao),'/', EXTRACT(MONTH FROM emissao))
+HAVING    COUNT(idpedido) > 5;
 
 -- 149. Mostre as categorias cujo preço médio dos produtos é maior que R$ 300,00 (descrição e média de precounitario).
+
 
 -- 150. Liste os clientes que compraram em mais de 2 datas diferentes (nome e contagem distinta de emissao).
 
